41798a7619445223be153db6e713a1ba6b74dbbb
danielmichaneck <daniel.michaneck@gmail.com>
2024-07-05 13:57:08 +0200

    Started on part 2.

--- wrong.js
+++ wrong.js
@@ -1,7 +1,9 @@
-
-
+/* ##### Set the logo text to the correct color ##### */
 const logoText = document.querySelector(".logo-text");
+console.log(logoText.style);
+logoText.style.color = "#384241";
 
-console.log(logoText);
 
-//logoText.style.color = #384241;
+/* ##### Set the logo text to the correct color ##### */
+const header = document.querySelector("header");
+header.style.alignItems = "flex-start";
